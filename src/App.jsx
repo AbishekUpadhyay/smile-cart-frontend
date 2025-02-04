6a5c3d4ac5d4762e74f91bf68b1fa14c7af4812d
BigBinary <bigbinary@BigBinarys-MacBook-Pro-3.local>
2025-02-04 18:19:35 +0530

    Added product listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
 import React from "react";
 
-import Home from "components/Home";
-import PageNotFound from "components/PageNotFound";
-import { NavLink, Route, Switch } from "react-router-dom";
+import { PageNotFound } from "components/commons";
+import ProductList from "components/ProductList";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import "./App.css";
 import Product from "./components/Product";
 
 const App = () => (
-  <>
-    <div className="flex space-x-2">
-      <NavLink exact activeClassName="underline font-bold" to="/">
-        Home
-      </NavLink>
-      <NavLink exact activeClassName="underline font-bold" to="/product">
-        Product
-      </NavLink>
-    </div>
-    <Switch>
-      <Route exact component={Product} path="/product" />
-      <Route exact component={Home} path="/" />
-      <Route component={PageNotFound} path="*" />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact component={ProductList} path="/products" />
+    <Route exact component={Product} path="/products/:slug" />
+    <Redirect exact from="/" to="/products" />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
 );
 export default App;
