a1f519efd831d038c61ce4e7698e16cd34fb4268
BigBinary <bigbinary@BigBinarys-MacBook-Pro-3.local>
2025-02-07 14:48:52 +0530

    Integrated Formik form into the checkout page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -7,6 +7,7 @@ import { isNotNil } from "ramda";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import routes from "routes";
+import i18n from "src/common/i18n";
 import withTitle from "utils/withTitle";
 
 import Carousel from "./Carousel";
@@ -60,4 +61,4 @@ const Product = () => {
   );
 };
 
-export default withTitle(Product);
+export default withTitle(Product, i18n.t("product"));
