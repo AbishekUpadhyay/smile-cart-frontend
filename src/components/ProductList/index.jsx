617a1a1c0553db29aab1282182d5bf16384107fb
BigBinary <bigbinary@BigBinarys-MacBook-Pro-3.local>
2025-02-07 10:23:53 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,46 +4,66 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [searchKey, setSearchKey] = useState("");
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
   const debouncedSearchKey = useDebounce(searchKey);
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const productParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productParams);
 
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className="flex flex-col">
-      <Header
-        shouldShowBackButton={false}
-        title="Smile Cart"
-        actionBlock={
-          <Input
-            placeholder="Search products"
-            prefix={<Search />}
-            type="search"
-            value={searchKey}
-            onChange={e => setSearchKey(e.target.value)}
-          />
-        }
-      />
-      {isEmpty(products) ? (
-        <NoData className="h-full w-full" title="No products to show" />
-      ) : (
-        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
-          {products.map(product => (
-            <ProductListItem key={product.slug} {...product} />
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div className="flex flex-col">
+        <Header
+          shouldShowBackButton={false}
+          title="Smile Cart"
+          actionBlock={
+            <Input
+              placeholder="Search products"
+              prefix={<Search />}
+              type="search"
+              value={searchKey}
+              onChange={e => {
+                setSearchKey(e.target.value);
+                setCurrentPage(DEFAULT_PAGE_INDEX);
+              }}
+            />
+          }
+        />
+        {isEmpty(products) ? (
+          <NoData className="h-full w-full" title="No products to show" />
+        ) : (
+          <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+            {products.map(product => (
+              <ProductListItem key={product.slug} {...product} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage || DEFAULT_PAGE_INDEX}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
+      </div>
+    </>
   );
 };
 export default withTitle(ProductList);
